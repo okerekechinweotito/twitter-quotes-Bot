88b9d365459c7385e29180c0550fe3c601279cc5
Okereke Chinweotito <okerekechinweotito@gmail.com>
2024-04-05 11:29:09 +0100

    bypass midjourney cloudfare bot protection

--- utils/downloadImage.js
+++ utils/downloadImage.js
@@ -27,20 +27,28 @@ const downloadImage = async (url, filepath) => {
 
 export { downloadImage };
  */
+
+import puppeteer from "puppeteer";
 import fs from "fs";
-import fetch from "node-fetch";
 
 const downloadImage = async (url, filepath) => {
   console.log(url);
-  const response = await fetch(url);
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-  const arrayBuffer = await response.arrayBuffer();
-  const buffer = Buffer.from(arrayBuffer);
-  fs.writeFile(filepath, buffer, () => {
+  const browser = await puppeteer.launch({
+    headless: "new",
+    defaultViewport: null,
+  });
+  const page = await browser.newPage();
+  let viewSource = await page.goto(`${url}`, {
+    waitUntil: "load",
+  });
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  viewSource = await page.reload({ waitUntil: "load" });
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  const buffer = await viewSource?.buffer();
+  fs.writeFile(`${filepath}`, buffer, () => {
     console.log("Image downloaded successfully!");
   });
+  await browser.close();
 };
 
 export { downloadImage };
