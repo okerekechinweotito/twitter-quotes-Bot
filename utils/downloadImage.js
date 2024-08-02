85c4f73fd2abde18b4a227d393c2a04faa20af37
Okereke Chinweotito <okerekechinweotito@gmail.com>
2024-08-02 20:46:02 +0100

    cleanup downloadimage util

--- utils/downloadImage.js
+++ utils/downloadImage.js
@@ -1,33 +1,3 @@
-// Use this scraping script when image cant be downloaded with fetch
-/* 
-import puppeteer from 'puppeteer';
-import fs from 'fs';
-
-const downloadImage = async (url, filepath) => {
-  console.log(url);
-  const browser = await puppeteer.launch({
-    headless: 'new',
-    defaultViewport: null,
-  });
-
-  const page = await browser.newPage();
-
-  const viewSource = await page.goto(`${url}`, {
-    waitUntil: 'domcontentloaded',
-  });
-
-  const buffer = await viewSource.buffer();
-
-  fs.writeFile(`${filepath}`, buffer, () => {
-    console.log('Image downloaded successfully!');
-  });
-
-  await browser.close();
-};
-
-export { downloadImage };
- */
-
 import puppeteer from "puppeteer";
 import fs from "fs";
 
